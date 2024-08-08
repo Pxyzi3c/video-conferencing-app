00b99243efda929e7c3f42898d8dc95a18c3978a
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-08-08 11:04:08 +0800

    configure clerk nextjs for authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
     return (
@@ -16,7 +17,9 @@ const Navbar = () => {
                 <p className='text-[26px] font-extrabold hidden sm:block'>Hoom</p>
             </Link>
             <div className="flex-between gap-5">
-                {/* Clerk - User Mangement */}
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
 
                 <MobileNav />
             </div>     
