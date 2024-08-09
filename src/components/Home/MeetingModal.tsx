288e98cac5337ef0099a96cc1cfc066102dfc767
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-08-09 13:38:44 +0800

    instant meeting link

--- src/components/Home/MeetingModal.tsx
+++ src/components/Home/MeetingModal.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { ReactNode, useState } from 'react'
 
-const MeetingModal = () => {
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
+import { cn } from '@/lib/utils';
+        
+interface MeetingModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    title: string;
+    className?: string;
+    children?: ReactNode;
+    handleClick?: () => void;
+    buttonText?: string;
+    image?: string;
+    buttonIcon?: string
+}
+
+const MeetingModal = ({ isOpen, onClose,  title, className, children, handleClick, buttonText, image, buttonIcon }: MeetingModalProps) => {
     return (
-        <div>MeetingModal</div>
+        <>
+            <Dialog 
+                header={title} 
+                visible={isOpen} 
+                style={{ width: '50vw' }} 
+                onHide={onClose} 
+                className='border-none'
+            >
+                <div 
+                    className={cn('flex flex-col gap-6 border-none')}
+                >
+                    fsdf
+                </div>
+            </Dialog>
+        </>
     )
 }
 
--- src/components/Home/MeetingTypeList.tsx
+++ src/components/Home/MeetingTypeList.tsx
@@ -1,26 +1,33 @@
 'use client'
 
 import React, { useState } from 'react';
+
 import { 
     Plus,
     Calendar,
     Video,
     UserRoundPlus
 } from 'lucide-react';
+
 import Card from './Card';
+import MeetingModal from './MeetingModal';
+
 import { useRouter } from 'next/navigation';
 
 const MeetingTypeList = () => {
     const router = useRouter();
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>();
 
+    const createMeeting = () => {
+
+    }
     return (
         <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
             <Card 
                 title="New Meeting"
                 icon={<Plus />}
                 description="Start an instant meeting"
-                handleClick={() => setMeetingState('isJoiningMeeting')}
+                handleClick={() => setMeetingState('isInstantMeeting')}
                 color="bg-orange-1"
             />
             <Card 
@@ -43,6 +50,15 @@ const MeetingTypeList = () => {
                 description="Via invitation link"
                 handleClick={() => setMeetingState('isJoiningMeeting')}
                 color="bg-yellow-1"
+            />
+
+            <MeetingModal 
+                isOpen={meetingState === 'isInstantMeeting'}
+                onClose={() => setMeetingState(undefined)}
+                title="Start an Instant Meeting"
+                className="text-center"
+                buttonText="Start Meeting"
+                handleClick={createMeeting}
             />
         </section>
     )
