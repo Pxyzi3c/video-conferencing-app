5120ee98920444f44f02640e823a3eef8bba03f5
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-08-10 09:22:37 +0800

    configure stream client provider

---
+++ providers/StreamClientProvider.tsx
@@ -0,0 +1,39 @@
+import { tokenProvider } from '@/actions/stream.action';
+import { useUser } from '@clerk/nextjs';
+import {
+    StreamVideo,
+    StreamVideoClient,
+} from '@stream-io/video-react-sdk';
+import { ReactNode, useEffect, useState } from 'react';
+  
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+    const [videoClient, setVideoClient] = useState<StreamVideoClient>();
+    const { user, isLoaded } = useUser();
+
+    useEffect(() => {
+        if(!user || !isLoaded) return;
+        if(!apiKey) throw new Error('Stream API key not found');
+
+        const client = new StreamVideoClient({
+            apiKey,
+            user: {
+                id: user?.id,
+                name: user?.username || user?.id,
+                image: user?.imageUrl,
+            },
+            tokenProvider,
+        })
+
+        setVideoClient(client)
+    }, [user, isLoaded])
+
+    return (
+        <StreamVideo client={videoClient}>
+
+        </StreamVideo>
+    );
+};
+
+export default StreamVideoProvider;
