767f1cea30f082e0820244b0e0da97bcd4003139
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-08-10 17:45:32 +0800

    setup meeting component

--- src/components/Meeting/Setup.tsx
+++ src/components/Meeting/Setup.tsx
@@ -1,8 +1,12 @@
+import { VideoPreview } from '@stream-io/video-react-sdk'
 import React from 'react'
 
 function MeetingSetup() {
     return (
-        <div>Meeting Setup</div>
+        <div className="flex h-screen w-full flex-col items-center justify-center gap-3">
+            <h1 className="text-2xl font-bold">Setup</h1>
+            <VideoPreview />
+        </div>
     )
 }
 
