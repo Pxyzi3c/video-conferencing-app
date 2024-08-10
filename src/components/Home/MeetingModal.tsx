693118448224870f21a2f0f5c202f2b810585ab0
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-08-10 08:27:11 +0800

    finish the stream modal and start with the start a meeting functionality

--- src/components/Home/MeetingModal.tsx
+++ src/components/Home/MeetingModal.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode, useState } from 'react'
 import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
         
 interface MeetingModalProps {
     isOpen: boolean;
@@ -29,7 +30,25 @@ const MeetingModal = ({ isOpen, onClose,  title, className, children, handleClic
                 <div 
                     className={cn('flex flex-col gap-6 border-none')}
                 >
-                    fsdf
+                    <div className="flex flex-col gap-6">
+                        {image && (
+                            <div className="flex justify-center">
+                                <Image
+                                    src={image}
+                                    alt="image"
+                                    width={72}
+                                    height={72}
+                                />
+                            </div>
+                        )}
+                        {children}
+                        <Button
+                            label={buttonText || 'Schedule Meeting'}
+                            icon={buttonIcon}
+                            className='ring-0'
+                        >
+                        </Button>
+                    </div>
                 </div>
             </Dialog>
         </>
