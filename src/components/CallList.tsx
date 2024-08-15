d36f981896ffaa57f93a6d0554e758b58bbd8a6b
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-08-15 13:04:15 +0800

    create useGetCalls hook and CallList component

---
+++ src/components/CallList.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useGetCalls } from '@/hooks/useGetCalls'
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
+import { Call, CallRecording } from '@stream-io/video-react-sdk';
+function CallList({ type }: { type: 'previous' |'upcoming' | 'recordings' }) {
+    const { previousCalls, upcomingCalls, isLoading } = useGetCalls();
+    const router = useRouter();
+    const [recordings, setRecordings] = useState<CallRecording[]>([]);
+
+    const getCalls = () => {
+        switch (type) {
+            case 'previous':
+                return previousCalls;
+            case 'upcoming':
+                return upcomingCalls;
+            case 'recordings':
+                return recordings;
+            default:
+                return [];
+        }
+    }
+
+    const getNoCallsMessage = () => {
+        switch (type) {
+            case 'previous':
+                return 'No Previous Calls';
+            case 'upcoming':
+                return 'No Upcoming Calls';
+            case 'recordings':
+                return 'No Recordings Found';
+            default:
+                return '';
+        }
+    }
+
+    const calls = getCalls();
+    const noCallsMessage = getNoCallsMessage();
+
+    return (
+        <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+            {calls && calls.length > 0 ? calls.map((meeting: Call | CallRecording) => (
+                <MeetingCard />
+            ))}
+        </div>
+    )
+}
+
+export default CallList
